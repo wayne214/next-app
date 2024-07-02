1f721978ca9518e328c33c86b47c13a2a9c73237
wayne214 <jun_wei_like1@163.com>
2024-07-02 21:27:36 +0800

    error.js

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,3 +1,7 @@
+'use client'
+
+import React, {useState} from 'react'
+
 async function getData() {
     await new Promise((resolve) => setTimeout(resolve, 2000))
     return {
@@ -6,5 +10,14 @@ async function getData() {
 }
 export default async function Page() {
     const {message} = await getData()
-    return <h1>{message}</h1>
+
+    const [error, setError] = useState(false)
+
+    const handleGetError = () => {
+        setError(true)
+    }
+
+    return (
+        <>(error ? Error(): <button onClick={handleGetError}>Get Error</button>)</>
+    )
 }
